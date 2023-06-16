4db348ea180b2f3a4d261732809ebf771170cae5
MatheusAndradeeee <mateusandracosta@gmail.com>
2023-06-16 10:42:54 -0400

    1

---
+++ conta/Conta.cs
@@ -0,0 +1,31 @@
+﻿using System.Drawing;
+using System.Runtime.CompilerServices;
+
+public class Conta
+{//atributos(caracteristicas)
+    public string nome;
+    public string tipoConta;
+    public double saldo;
+
+     
+      
+    public void Deposito(double deposito)
+    {
+        if (deposito > 0)
+        {
+            saldo += deposito;
+        }
+        else { Console.WriteLine("Valor inválido."); }
+       
+    }
+
+    public void Sacar(double saque) 
+    {
+         if (saque <= saldo)
+        {
+            saldo -= saque;
+        }
+        else { Console.WriteLine("Valor inválido."); }
+
+    }
+}
---
+++ conta/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+
+public class Programa
+{
+    static void Main(string[] args)
+    {
+
+        Conta c = new Conta();
+        Console.WriteLine("Dígite seu nome: ");
+        c.nome = Console.ReadLine();
+
+        Console.WriteLine("Dígite o tipo da conta(Corrente ou Poupança):");
+        c.tipoConta = Console.ReadLine();
+
+
+        c.saldo = 0;
+
+        Console.WriteLine($"{c.nome}, dígite o valor para ser depositado:");
+        double valor = Convert.ToDouble(Console.ReadLine());
+       
+            c.Deposito(valor);
+        
+        Console.WriteLine($"{c.nome}, sua conta {c.tipoConta} tem {c.saldo.ToString("c2")}");
+
+        Console.WriteLine("Dígite um valor para sacar!");
+        double valorSaque = Convert.ToDouble(Console.ReadLine());
+        c.Sacar(valorSaque);
+        Console.WriteLine($"{c.nome}, sua conta {c.tipoConta} tem {c.saldo.ToString("c2")}");
+        Console.ReadKey();
+    }
+}
